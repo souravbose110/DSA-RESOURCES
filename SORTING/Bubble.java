dee4d908bf0a62ab5c135e659e000275accb31ea
Sourav Bose <60739372+souravbose110@users.noreply.github.com>
2021-08-11 17:02:38 +0530

    Add files via upload

--- SORTING/Bubble.java
+++ SORTING/Bubble.java
@@ -1,1 +1,12 @@
-
+public static void bubble (int[] a) {
+	int n = a.length;
+	for (int i=1; i<n; ++i) {
+		for (int j=0; j<n-i; ++j) {
+			if (arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
